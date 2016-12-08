da0c552def78f27fea44870c07ec6b760bc01c0b
Tony Vu <AnthonyVuOD@gmail.com>
2016-12-07 19:42:48 -0600

    initial push

---
+++ js/portfolio.js
@@ -0,0 +1,18 @@
+new Vue({
+  el: '#vue',
+  data: {
+    message: 'Hello Vue.js!'
+  }
+})
+
+new Vue ({
+  el: '#portfolio',
+  data: {
+    projects: [
+      {name: "Greenbelt Climbing Routes" , link:"https://anthonyvuod.github.io/GreenbeltClimbingRoutes/", photo:"images/greenbeltSS2.jpg"},
+      {name: "Travis Perkins Photography" , link:"https://anthonyvuod.github.io/TravisPerkinsPhotography/", photo:"images/travisPerkinsSS2.jpg"},
+      {name: "How Much House?", link:"https://anthonyvuod.github.io/HowMuchHouse/", photo:"images/houseSS2.jpg"},
+      {name: "Vu Properties", link:"https://anthonyvuod.github.io/VuProperties/", photo: "images/vuPropertiesSS2.jpg"}
+    ]
+  }
+})
