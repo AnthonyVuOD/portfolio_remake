d36c7376a18d96ef1a43b4d3b4c4498be5541a17
Tony Vu <AnthonyVuOD@gmail.com>
2016-12-13 20:17:29 -0600

    update

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,28 +1,18 @@
 new Vue({
   el: '#landingPage',
-  methods: {
-    parallax: function (){
-      var landingDiv = document.getElementById('landingDiv');
-      // landingDiv.style.bottom = window.pageYOffset * .2 + "px";
-      console.log("hello");
+  created: function () {
+            window.addEventListener('scroll', function(){
+              console.log('hello');
+            })
     }
-  },
-  ready: function(){
-     window.addEventListener("scroll", function(){
-       console.log("bello");
-     });
-  }
-  // ready() {
-  //   window.addEventListener("scroll", parallax, false);
-  // }
 })
 
 new Vue ({
   el: '#portfolio',
   data: {
     projects: [
-      {name: "Greenbelt Climbing Routes" , link:"https://anthonyvuod.github.io/GreenbeltClimbingRoutes/", photo:"images/greenbeltSS2.jpg"},
-      {name: "Travis Perkins Photography" , link:"https://anthonyvuod.github.io/TravisPerkinsPhotography/", photo:"images/travisPerkinsSS2.jpg"},
+      {name: "Greenbelt Climbing Routes", link:"https://anthonyvuod.github.io/GreenbeltClimbingRoutes/", photo:"images/greenbeltSS2.jpg"},
+      {name: "Travis Perkins Photography", link:"https://anthonyvuod.github.io/TravisPerkinsPhotography/", photo:"images/travisPerkinsSS2.jpg"},
       {name: "How Much House?", link:"https://anthonyvuod.github.io/HowMuchHouse/", photo:"images/houseSS2.jpg"},
       {name: "Vu Properties", link:"https://anthonyvuod.github.io/VuProperties/", photo: "images/vuPropertiesSS2.jpg"}
     ]
